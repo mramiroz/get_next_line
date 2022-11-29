123f2d6da36d3b86982d5d5e736c4811e49714e5
Manuel Ramiro Zabaleta <mramiro-@c3r9s3.42madrid.com>
2022-11-29 13:04:26 +0100

    Otro coommit

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mramiro- <mramiro-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 10:50:04 by mramiro-          #+#    #+#             */
-/*   Updated: 2022/11/29 10:26:35 by mramiro-         ###   ########.fr       */
+/*   Updated: 2022/11/29 12:55:51 by mramiro-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ char	*copiarenout(char *buffer)
 	char			*out;
 	int 	size;
 	int				n;
-	
+
 	size = 0;
 	size += BUFFER_SIZE;
 	i = 0;
@@ -53,24 +53,23 @@ char	*copiarenout(char *buffer)
 	return (out);
 }
 
-char	*readdoc(char *out, int fd)
+char	*readdoc(char *buffer, int fd)
 {
-	char	*buffer;
+	char	*out;
 	char	*temp;
 	int		numbytes;
 
 	buffer = malloc(BUFFER_SIZE);
+	if (!buffer)
+		return (0);
 	numbytes = read(fd, buffer, BUFFER_SIZE);
-	if (searchn(buffer) != 0)
-	{
-		out = copiarenout(buffer);
-		free(buffer);
-		return (out);
-	}
+	if (numbytes == -1)
+		return (0);
 	out = copiarenout(buffer);
-	while (searchn(buffer) == 0 && searchn(out) == 0)
+	while (searchn(buffer) == 0)
 	{
-		read(fd, buffer, BUFFER_SIZE);
+		numbytes = read(fd, buffer, BUFFER_SIZE);
+		printf("%s\n", buffer);
 		temp = ft_strjoin(out, buffer);
 		out = ft_strdup(temp);
 		free(temp);
@@ -78,10 +77,19 @@ char	*readdoc(char *out, int fd)
 	free(buffer);
 	return (out);
 }
-
+//char* updateOut(int fd, char *buffer)
+//{
+//	int size;
+//	static char *out;
+//
+//	size = ft_strlen(buffer);
+//	read(fd, out + size, BUFFER_SIZE);
+//	return (out);
+//}
 char	*get_next_line(int fd)
 {
 	static char	*out;
+	char *temp;
 
 	if (read(fd, 0, 0) < 0)
 	{
@@ -94,15 +102,17 @@ char	*get_next_line(int fd)
 	}
 	if (fd < 0 || BUFFER_SIZE < 0)
 		return (NULL);
-	out = readdoc(out, fd);
-	if (!out)
-		return (0);
-	return (out);
+	temp = readdoc(out, fd);
+	read(fd, out, BUFFER_SIZE);
+	printf("%s", out);
+	return (temp);
 }
 
 int main()
 {
 	int fd = open("archivo.txt", O_RDONLY);
+	printf("%s", get_next_line(fd));
+	fd = open("archivo.txt", O_RDONLY);
 	printf("%s", get_next_line(fd));
 	//char *buffer = malloc(BUFFER_SIZE);
 	//read(fd, buffer, BUFFER_SIZE);
