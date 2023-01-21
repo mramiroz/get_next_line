4b47c07e2e17fb1db8c3ab15a072277f16ce71c9
mramiroz <manuel.ramiro.zabaleta@gmail.com>
2023-01-22 00:46:01 +0100

    Nuevo intento de gnl

---
+++ get_next_line.c
@@ -0,0 +1,122 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrarmiro- <mramiro-@student.42madrid.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/17 16:40:12 by mrarmiro-         #+#    #+#             */
+/*   Updated: 2023/01/17 16:40:12 by mrarmiro-        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int searchn(char *c)
+{
+    int i;
+
+    i = 0;
+    while(c[i] != '\0')
+    {
+        if(c[i] == '\n')
+            return(i);
+        i++;
+    }
+    return (0);
+}
+
+char *readdoc(int fd)
+{
+    char *out;
+    char *buffer;
+    int nbytes;
+
+    nbytes = 1;
+    out = ft_calloc(1, sizeof(char));
+    buffer = ft_calloc(1, sizeof(char));
+    
+    while(nbytes > 0 && searchn(out) == 0)
+    {
+        nbytes = read(fd, buffer, BUFFER_SIZE);
+        if (nbytes == 0)
+            break ;
+        out = ft_strjoin(out, buffer);
+    }
+    free(buffer);
+    return (out);
+}
+
+char *getout(char *buffer)
+{
+    int i;
+    char *out;
+
+    i = 0;
+    out = ft_calloc(searchn(buffer), sizeof(char));
+    while (buffer[i] != '\n' && buffer[i] != '\0')
+    {
+        out[i] = buffer[i];
+        i++;
+    }
+    out[i] = '\0';
+    free(buffer);
+    return (out);
+
+}
+
+char *cutn(char *c)
+{
+    int i;
+    int x;
+    char *out;
+
+    i = searchn(c) + 1;
+    x = ft_strlen(c) - i;
+    out = ft_calloc(x + 1, sizeof(char));
+    x = 0;
+    while(c[i] != '\0')
+        out[x++] = c[i++];
+    return (out);
+}
+
+char *get_next_line(int fd)
+{
+    char *out;
+    static char *temp;
+
+    if (!temp || searchn(temp) == 0)
+    {
+        out = readdoc(fd);
+        if(temp != NULL)
+            out = ft_strjoin(temp, out);
+        temp = cutn(out);
+        out = getout(out);
+    }
+    else
+    {
+        out = getout(temp);
+        temp = cutn(temp);
+    }
+    return (out);
+}
+
+
+
+int main()
+{
+	int fd = open("archivo.txt", O_RDONLY);
+	printf("Return: %s\n", get_next_line(fd));
+	printf("Return: %s\n", get_next_line(fd));
+	printf("Return: %s\n", get_next_line(fd));
+	printf("Return: %s\n", get_next_line(fd));
+
+
+
+	system("leaks -q a.out");
+}
---
+++ get_next_line.h
@@ -0,0 +1,18 @@
+#ifndef GET_NEXT_LINE_H
+
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 50
+# endif
+    int	ft_strlen(const char *str);
+    void	*ft_calloc(size_t num, size_t size);
+    char	*ft_strjoin(char const *s1, char const *s2);
+    int searchn(char *c);
+    char *readdoc(int fd);
+    char *getout(char *buffer);
+    char *cutn(char *c);
+    char *get_next_line(int fd);
+#endif
